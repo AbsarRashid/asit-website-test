4eda0e228fd4d447b5de1d2cf9a32f710a783bf2
AbsarRashid <50824150+AbsarRashid@users.noreply.github.com>
2020-09-01 19:02:55 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,6 @@ $(".main-left-container__nav-border li").on("click", function () {
   var scrollAnchor = $(this).attr("data-scroll"),
     scrollPoint =
       $('section[data-anchor="' + scrollAnchor + '"]').offset().top - vh(15);
-  console.log($('section[data-anchor="' + scrollAnchor + '"]').offset().top);
   $("body, html").animate(
     {
       scrollTop: scrollPoint,
@@ -93,7 +92,6 @@ $(".main-right-container__dot-nav ul li").on("click", function () {
 $(window)
   .scroll(function () {
     var windscroll = $(window).scrollTop();
-    console.log(windscroll);
     if (windscroll >= 100) {
       $(".main-center-container section").each(function (i) {
         if ($(this).position().top <= windscroll + 100) {
@@ -103,16 +101,56 @@ $(window)
           $(".main-right-container__dot-nav ul li.current").removeClass(
             "current"
           );
+          $(".menu-nav__item.current").removeClass("current");
           $(".main-left-container__nav-border li").eq(i).addClass("current");
           $(".main-right-container__dot-nav ul li").eq(i).addClass("current");
+          $(".menu-nav__item").eq(i).addClass("current");
         }
       });
     } else {
       $(".main-left-container__nav-border li.current").removeClass("current");
       $(".main-right-container__dot-nav ul li.current").removeClass("current");
+      $(".menu-nav__item.current").removeClass("current");
       $(".main-right-container__dot-nav ul li:first").addClass("current");
-
       $(".main-left-container__nav-border li:first").addClass("current");
+      $(".menu-nav__item:first").addClass("current");
     }
   })
   .scroll();
+
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".menu-btn__overlay");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+$(".menu-nav__item").on("click", function () {
+  var scrollAnchor = $(this).attr("data-scroll"),
+    scrollPoint =
+      $('section[data-anchor="' + scrollAnchor + '"]').offset().top - vh(15);
+  $("body, html").animate(
+    {
+      scrollTop: scrollPoint,
+    },
+    "slow"
+  );
+  toggleMenu();
+});
+
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+    showMenu = false;
+  }
+}
